b8d56ce1b14b9ab8c6b524114ef52cceb11b4b62
TonyBean <38479405+TonyBean-Dong@users.noreply.github.com>
2025-01-14 14:37:40 +0800

    修改音视频案例

--- Samples~/MediaManager/AudioManagerDemo.cs
+++ Samples~/MediaManager/AudioManagerDemo.cs
@@ -5,7 +5,7 @@ namespace NonsensicalKit.UGUI.Media.Samples
     public class AudioManagerDemo : MonoBehaviour
     {
         [SerializeField] private AudioManager m_audioManager;
-        [SerializeField] private string m_audioUrl = "http://music.163.com/song/media/outer/url?id=1270558.mp3";
+        [SerializeField] private string m_audioUrl = "https://music.163.com/song/media/outer/url?id=1492276411.mp3";
 
         public void PlayTest()
         {
